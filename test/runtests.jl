7e30813e38840f99c79b532190feb9ecd4e8edfc
rex <1073853456@qq.com>
2023-03-26 00:20:13 +0800

    add test for proxy

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using OpenAICall
 using Test
 
+include("tst_proxy.jl")
+
 @testset "OpenAICall.jl" begin
     # Write your tests here.
 end
---
+++ test/tst_proxy.jl
@@ -0,0 +1,23 @@
+
+@testset "proxy_on" begin
+    url = "127.0.0.1:7890"
+    proxy_on(url, url)
+    @test ENV["HTTP_PROXY"] == url
+    @test ENV["HTTPS_PROXY"] == url
+end
+
+@testset "proxy_off" begin
+    proxy_off()
+    @test !haskey(ENV, "HTTP_PROXY")
+    @test !haskey(ENV, "HTTPS_PROXY")
+end
+
+@testset "proxy_status" begin
+    url = "127.0.0.1:7890"
+    proxy_on(url, url)
+    @test ENV["HTTP_PROXY"] == url
+    @test ENV["HTTPS_PROXY"] == url
+    proxy_status()
+    proxy_off()
+    @test true
+end
