355773c6b7fe873623f7de4c7e16d4f72f161b98
Documenter.jl <documenter@juliadocs.github.io>
2023-03-25 06:19:56 +0000

    build based on f54d85d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OpenAICall","category":"page"},{"location":"#OpenAICall","page":"Home","title":"OpenAICall","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OpenAICall.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OpenAICall]","category":"page"}]
+}
